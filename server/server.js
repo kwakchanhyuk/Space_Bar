6cf2777d3904fc56a89133d451a4330d036a7332
kim chae yeon <chae9750@gmail.com>
2022-09-16 11:23:39 +0900

    node express 서버에 react 연동

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
-const test = require('.//Router/test');
+const path = require('path');
 
-app.use('/api', test);
+app.listen(8080, function () {
+  console.log('listening on 8080')
+}); 
 
-const port=5000;
-app.listen(port, ()=>{console.log(`Listening on port ${port}`)});
+app.use(express.static(path.join(__dirname, 'nft-market/build')));
+
+app.get('/', function(res){
+    res.sendFile(path.join(__dirname, 'nft-market/build/index.html'));
+})
