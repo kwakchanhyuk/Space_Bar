1a7c6cbcd866562aacef267f01635fd8cab144ea
yeanajun <qkqlalsk@naver.com>
2022-09-26 22:41:44 +0900

    game 연동

--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'nft-market/build/index.html'));
 })
 
+app.use(express.static(path.join(__dirname, 'game')));
+
 app.get('/game', function(req, res){
   res.sendFile(path.join(__dirname, 'game/coding/code/index.html'));
 })
